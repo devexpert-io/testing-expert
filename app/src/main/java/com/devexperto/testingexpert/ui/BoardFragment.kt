2b46a35315b1d6c4790cf50c59a35a0d47250374
antoniolg <antonio@devexperto.com>
2022-12-21 13:48:22 +0100

    Setup Fragment board with navigation

---
+++ app/src/main/java/com/devexperto/testingexpert/ui/BoardFragment.kt
@@ -0,0 +1,9 @@
+package com.devexperto.testingexpert.ui
+
+import androidx.fragment.app.Fragment
+import com.devexperto.testingexpert.R
+
+class BoardFragment : Fragment(R.layout.fragment_board) {
+
+
+}
