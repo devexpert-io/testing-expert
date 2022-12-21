f8adbaeebddc40446a2b9802d4a9e6fadc1c3f7f
antoniolg <antonio@devexperto.com>
2022-12-21 13:44:20 +0100

    Initial Commit

---
+++ app/src/main/java/com/devexperto/testingexpert/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devexperto.testingexpert.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devexperto.testingexpert.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
