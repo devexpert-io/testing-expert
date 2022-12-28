03cb8c38667c87d710d912347959ad4f99ae7bed
antoniolg <antonio@devexperto.com>
2022-12-28 01:19:19 +0100

    Build bottom navigation

--- app/src/main/java/com/devexperto/testingexpert/ui/MainActivity.kt
+++ app/src/main/java/com/devexperto/testingexpert/ui/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.devexperto.testingexpert.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.devexperto.testingexpert.R
+import com.devexperto.testingexpert.databinding.ActivityMainBinding
+import com.devexperto.testingexpert.ui.board.BoardFragment
+import com.devexperto.testingexpert.ui.scoreboard.ScoreboardFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+
+            bottomNavigation.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.navigation_board -> {
+                        supportFragmentManager.commit {
+                            replace(R.id.app_container, BoardFragment())
+                        }
+                        true
+                    }
+                    R.id.navigation_scoreboard -> {
+                        supportFragmentManager.commit {
+                            replace(R.id.app_container, ScoreboardFragment())
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            bottomNavigation.selectedItemId = R.id.navigation_board
+        }
     }
 }
