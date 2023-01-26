e1bdf947a9e79f4f8c01e372c5fb201b444ad19b
antoniolg <antonio@devexperto.com>
2023-01-26 11:57:44 +0100

    Compose Tests - Cell
    (cherry picked from commit aeab0502f230413027d2a07cc410038f9652744b)

---
+++ app/src/androidTest/java/com/devexperto/testingexpert/composeui/board/CellUnitTest.kt
@@ -0,0 +1,53 @@
+package com.devexperto.testingexpert.composeui.board
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.devexperto.testingexpert.domain.Empty
+import com.devexperto.testingexpert.domain.X
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CellUnitTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private var onClickCalled = false
+    private val onClick: () -> Unit = { onClickCalled = true }
+
+    @Before
+    fun setUp() {
+        onClickCalled = false
+    }
+
+    @Test
+    fun whenCellIsEmpty_ItCanBeClicked(): Unit = with(composeTestRule) {
+        setContent {
+            Row {
+                Cell(Empty, onClick)
+            }
+        }
+
+        onNodeWithText(Empty.toString()).performClick()
+
+        assertTrue(onClickCalled)
+    }
+
+    @Test
+    fun whenCellIsNotEmpty_ItCannotBeClicked(): Unit = with(composeTestRule) {
+        setContent {
+            Row {
+                Cell(X, onClick)
+            }
+        }
+
+        onNodeWithText(X.toString()).performClick()
+
+        assertFalse(onClickCalled)
+    }
+}
